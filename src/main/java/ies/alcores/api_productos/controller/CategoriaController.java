46b0f507f3790061aa50a1cb95e5837da021e96d
ojeda0 <abrahamojeda09@gmail.com>
2026-02-23 10:12:35 +0100

    Endpoint listar categoria por ID

--- src/main/java/ies/alcores/api_productos/controller/CategoriaController.java
+++ src/main/java/ies/alcores/api_productos/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import ies.alcores.api_productos.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,14 @@ public class CategoriaController {
 
         return ResponseEntity.ok(this.categoriaService.findAll());
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> obtenerPorID(@PathVariable long id) {
+
+        return this.categoriaService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
--- src/main/java/ies/alcores/api_productos/service/CategoriaService.java
+++ src/main/java/ies/alcores/api_productos/service/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,12 @@ public class CategoriaService {
     public List<Categoria> findAll() {
 
         return this.categoriaRepository.findAll();
+
+    }
+
+    public Optional<Categoria> findById(Long id) {
+
+        return this.categoriaRepository.findById(id);
 
     }
 
