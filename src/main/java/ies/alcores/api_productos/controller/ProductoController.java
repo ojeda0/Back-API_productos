1719acfaaf53d91f36b4f51bfc4f8b63f5bea45b
ojeda0 <abrahamojeda09@gmail.com>
2026-02-23 09:11:07 +0100

    adios

--- src/main/java/ies/alcores/api_productos/controller/ProductoController.java
+++ src/main/java/ies/alcores/api_productos/controller/ProductoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/productos")
 public class ProductoController {
-//hola
+
     @Autowired
     private ProductoService productoService;
 
